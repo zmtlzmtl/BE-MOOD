c8a545862030bff45ec70591a1847339c3e0a235
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-13 09:47:18 +0900

    feat: index에 review 라우터 연결

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const UserRouter = require("./user.routes")
+const ReviewRouter = require("./review.route");
 
 router.use("/",UserRouter)
+router.use("/music", ReviewRouter);
 
 router.get("/", (_req, res) => {
     res.send("정상적으로 요청되었습니다.");
