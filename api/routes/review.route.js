6866aae5574e1737e015daed01400ef0fbaf35d7
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-13 14:13:49 +0900

    feat: 유효성검사 및 라우터에 유효성 미들웨어 추가

--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -4,13 +4,28 @@ const router = express.Router();
 const CommentController = require("../controllers/review.controller");
 const commentController = new CommentController();
 //로그인 미들웨어 자리
-//유효성 검증 자리
+const Validation = require("../middlewares/validation.middleware");
 
-router.post("/:musicId/review", commentController.addMusicReview);
-router.get("/:musicId/review", commentController.getMusicReview);
-router.put("/:musicId/review/:reviewId", commentController.updateMusicReview);
+router.post(
+  "/:musicId/review",
+  Validation.paramGetCheck,
+  Validation.reviewCheck,
+  commentController.addMusicReview
+);
+router.get(
+  "/:musicId/review",
+  Validation.paramGetCheck,
+  commentController.getMusicReview
+);
+router.put(
+  "/:musicId/review/:reviewId",
+  Validation.paramCheck,
+  Validation.reviewCheck,
+  commentController.updateMusicReview
+);
 router.delete(
   "/:musicId/review/:reviewId",
+  Validation.paramCheck,
   commentController.deleteMusicReview
 );
 
