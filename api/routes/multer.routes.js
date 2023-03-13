03f6c5167f051441297360ac3a002e5ab5d260ab
kwakseungmin23 <lookingforfandl@gmail.com>
2023-03-13 15:48:34 +0900

    feat: Music Upload to S3 & Create music to MySQL

--- api/routes/multer.routes.js
+++ api/routes/multer.routes.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const multer = require("multer");
-const { s3Upload } = require("../services/multerService");
+const { S3 } = require("aws-sdk");
+const { memoryStorage } = require("multer");
+const { Musics } = require("../../db/models/");
 router = express.Router();
 
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage(); //require buffer
 
-router.post("/upload", multer(storage).any(), async (req,res) => {
-    try{
-        const text = req.body
-        const results = await s3Upload(req.files, text);
-        console.log(results);
-        return res.status(200).json({results});
-    }catch(err){
-        console.log(err);
-        return res.status(400).json({err:err.message});
-    }
+s3Upload = async (file) => {
+  const s3 = new S3();
+  const param = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: file.originalname,
+    Body: file.buffer,
+  };
+  return s3.upload(param).promise();
+};
+
+router.post("/music", multer({ storage }).any(), async (req, res) => {
+  try {
+    const file = req.files[0];
+    const result = await s3Upload(file);
+    let userId = 1;
+    let Url = result.Location;
+    let { musicTitle, musicContent, status } = req.body;
+    await Musics.create({
+      musicUrl: Url,
+      musicTitle,
+      musicContent,
+      status,
+      userId,
+    });
+    res.json();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ err: err.message });
+  }
 });
 
 module.exports = router;
