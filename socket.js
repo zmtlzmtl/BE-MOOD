db350f682b66498cf18bdbe61c0247df6096acbd
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-14 15:39:56 +0900

    feat: 소켓을 이용한 다중채팅방 까지 구현

--- socket.js
+++ socket.js
@@ -23,7 +23,7 @@ app.use('/css', express.static('./static/css'))
 app.use('/js', express.static('./static/js'))
 
 /* Get 방식으로 / 경로에 접속하면 실행 됨 */
-app.get('/', function(request, response) {
+app.get('/rooms/:roomId', function(request, response) {
   fs.readFile('./static/index.html', function(err, data) {
     if(err) {
       response.send('에러')
@@ -36,35 +36,44 @@ app.get('/', function(request, response) {
 })
 
 io.sockets.on('connection', function(socket) {
-
+  const req = socket.request;
+  const {
+    headers: { referer }, //referer: http://localhost:8080/rooms/1
+  } = req;
+  const roomId = referer.split('/')[referer.split('/').length - 1] // roomId: 1
+  socket.join(roomId);  //room을 통해 소켓분리
+  
   /* 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 */
-  socket.on('newUser', function(name) {
-    console.log(name + ' 님이 접속하였습니다.')
+  socket.on('newUser', function(nickname) {
+    console.log(nickname + ' 님이 접속하였습니다.')
 
     /* 소켓에 이름 저장해두기 */
-    socket.name = name
+    socket.nickname = nickname
 
     /* 모든 소켓에게 전송 */
-    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
+    io.sockets.to(roomId).emit('update', {type: 'connect', nickname: 'SERVER', message: nickname + '님이 접속하였습니다.'})
   })
 
   /* 전송한 메시지 받기 */
   socket.on('message', function(data) {
     /* 받은 데이터에 누가 보냈는지 이름을 추가 */
-    data.name = socket.name
+    data.nickname = socket.nickname
     
     console.log(data)
 
     /* 보낸 사람을 제외한 나머지 유저에게 메시지 전송 */
-    socket.broadcast.emit('update', data);
+    socket.to(roomId).emit('update', data);
   })
 
   /* 접속 종료 */
   socket.on('disconnect', function() {
-    console.log(socket.name + '님이 나가셨습니다.')
+    console.log(socket.nickname + '님이 나가셨습니다.')
 
     /* 나가는 사람을 제외한 나머지 유저에게 메시지 전송 */
-    socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
+    socket.to(roomId).emit('update', {type: 'disconnect', nickname: 'SERVER', message: socket.nickname + '님이 나가셨습니다.'});
+    const currentRoom = socket.adapter.rooms[roomId];
+    const userCount = currentRoom ? currentRoom.length : 0;
+    console.log(userCount)
   })
 })
 
