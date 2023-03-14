8348174bf4074741aadd11f916aeecccd874a908
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-14 16:22:44 +0900

    init: socket.io 채팅방 모델구현

---
+++ db/migrations/20230311014425-create-chats.js
@@ -0,0 +1,38 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Chats", {
+      chatId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      roomId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      nickname: { //소셜로그인이 어떨지 모르겠네 model도
+        type: Sequelize.STRING,
+      },
+      massege: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Chats");
+  },
+};
