3bef59017ea827da3a4d63a35f037cbc549cf48e
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-16 16:04:32 +0900

    fix: try catch를 이용한 에러 잡기

--- socket.js
+++ socket.js
@@ -9,41 +9,56 @@ module.exports = (server) => {
     },
   });
   io.sockets.on("connection", async (socket) => {
-    socket.on("roomId", async function (roomId) {
-      socket.roomId = roomId;
-      socket.join(roomId);
-
-      // roomId가 등록되고 나서 findRoomChats를 실행합니다.
-      const findRoomChats = await Chats.findAll({
-        where: { roomId: socket.roomId },
-        order: [["createdAt", "ASC"]],
-        limit: 30,
+    try {
+      socket.on("roomId", async function (roomId) {
+        socket.roomId = roomId;
+        socket.join(roomId);
+  
+        // roomId가 등록되고 나서 findRoomChats를 실행합니다.
+        const findRoomChats = await Chats.findAll({
+          where: { roomId: socket.roomId },
+          order: [["createdAt", "ASC"]],
+          limit: 30,
+        });
+        console.log(`접속자: ${socket.id}`);
+        socket.emit("receive", findRoomChats);
       });
-      console.log(`접속자: ${socket.id}`);
-      socket.emit("receive", findRoomChats);
-    });
-    socket.on("newUser", function (nickname) {
-      console.log(nickname + " 님이 접속하였습니다.");
-      socket.nickname = nickname;
-      socket.to(socket.roomId).emit("onUser", nickname);
-    });
-    socket.on("sendMessage", function (data) {
-      data.nickname = socket.nickname;
-      console.log(data);
-      // 메세지가 안들어왔을때 거르는 방법 고안
-      // if (data.message.length === 0) {
-
-      // }
-      Chats.create({
-        roomId: socket.roomId,
-        nickname: data.nickname,
-        message: data.message,
+  
+      socket.on("newUser", function (nickname) {
+        console.log(nickname + " 님이 접속하였습니다.");
+        socket.nickname = nickname;
+        socket.to(socket.roomId).emit("onUser", nickname);
       });
-      socket.to(socket.roomId).emit("receiveMessage", data);
-    });
-    socket.on("disconnect", function () {
-      console.log(socket.nickname + "님이 나가셨습니다.");
-      socket.to(socket.roomId).emit("offUser", socket.nickname);
-    });
+  
+      socket.on("sendMessage", function (data) {
+        try {
+          if (!data.message) {
+            throw new Error("메시지가 입력되지 않았습니다.");
+          }
+  
+          data.nickname = socket.nickname;
+          console.log(data);
+  
+          Chats.create({
+            roomId: socket.roomId,
+            nickname: data.nickname,
+            message: data.message,
+          });
+  
+          socket.to(socket.roomId).emit("receiveMessage", data);
+        } catch (err) {
+          console.error(err);
+          socket.emit("errorMessage", err.message);
+        }
+      });
+  
+      socket.on("disconnect", function () {
+        console.log(socket.nickname + "님이 나가셨습니다.");
+        socket.to(socket.roomId).emit("offUser", socket.nickname);
+      });
+    } catch (err) {
+      console.error(err);
+      socket.emit("errorMessage", "서버 에러가 발생했습니다.");
+    }
   });
 };
