c14f1511dfbeaaff2e21d9c1b42986b56e6f36d1
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-20 18:46:37 +0900

    fix: route경로 중복적용

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,13 +5,15 @@ const UserRouter = require("./user.routes");
 const ReviewRouter = require("./review.routes");
 const ReCommentRouter = require("./recomment.routes");
 const Composer = require("./composer.routes")
+const LikeRouter = require("./like.route");
+const ScrapRouter = require("./scrap.route");
 
 router.use("/", UserRouter);
-router.use("/music", ReviewRouter);
-router.use("/review",ReCommentRouter)
+router.use("/review", ReCommentRouter);
+router.use("/music", [ScrapRouter, LikeRouter, ReviewRouter]);
 router.use("/", MusicRouter, Composer);
+
 router.get("/", (_req, res) => {
   res.send("정상적으로 요청되었습니다.");
 });
-
 module.exports = router;
