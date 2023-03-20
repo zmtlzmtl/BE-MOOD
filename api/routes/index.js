bd5e6a2b104e51db15a7568f04c3cd5d635fc218
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-20 18:48:19 +0900

    fix: route경로 재변경

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,15 +5,13 @@ const UserRouter = require("./user.routes");
 const ReviewRouter = require("./review.routes");
 const ReCommentRouter = require("./recomment.routes");
 const Composer = require("./composer.routes")
-const LikeRouter = require("./like.route");
-const ScrapRouter = require("./scrap.route");
 
 router.use("/", UserRouter);
-router.use("/review", ReCommentRouter);
-router.use("/music", [ScrapRouter, LikeRouter, ReviewRouter]);
+router.use("/music", ReviewRouter);
+router.use("/review",ReCommentRouter)
 router.use("/", MusicRouter, Composer);
-
 router.get("/", (_req, res) => {
   res.send("정상적으로 요청되었습니다.");
 });
+
 module.exports = router;
