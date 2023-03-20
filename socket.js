7740a614422085379ee8ccc27accd691203bc8cd
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-20 17:20:13 +0900

    fix: 무한스크롤에서 프론트와 index넘겨 다음 채팅 get 요청 완료

--- socket.js
+++ socket.js
@@ -10,27 +10,43 @@ module.exports = (server) => {
   });
   io.sockets.on("connection", async (socket) => {
     try {
-      socket.on("roomId", async function (roomId, index) {
+      socket.on("roomId", async function (roomId) {
+        console.log(`roomId : ${roomId}`);
         socket.roomId = roomId;
         socket.join(roomId);
-        const offset = index ? parseInt(index) : 0;
         // roomId가 등록되고 나서 findRoomChats를 실행합니다.
         const chats = await Chats.findAll({
           where: { roomId: socket.roomId },
           order: [["createdAt", "DESC"]],
           limit: 30,
-          offset: offset
         });
-        const findRoomChats = chats.reverse()
+        const findRoomChats = chats.reverse();
         console.log(`접속자: ${socket.id}`);
         socket.emit("receive", findRoomChats);
+      });
+      socket.on("scroll", async function (index) {
+        console.log(index + "스크롤이벤트발생");
+        const offset = (index - 1) * 30;
+        if (index !== 1) {
+          const chats = await Chats.findAll({
+            where: { roomId: socket.roomId },
+            order: [["createdAt", "DESC"]],
+            limit: 30,
+            offset: offset,
+          });
+          const findRoomChats = chats.reverse();
+          console.log(findRoomChats[0]);
+          return socket.emit("plusScroll", findRoomChats);
+        } else {
+          return;
+        }
       });
       socket.on("newUser", function (nickname) {
         console.log(nickname + " 님이 접속하였습니다.");
         socket.nickname = nickname;
 
         socket.emit("onUser", nickname);
-        socket.to(socket.roomId).emit("onUser", nickname)
+        socket.to(socket.roomId).emit("onUser", nickname);
       });
       socket.on("sendMessage", function (data) {
         data.nickname = socket.nickname;
@@ -40,8 +56,7 @@ module.exports = (server) => {
           nickname: data.nickname,
           message: data.message,
         });
-        console.log(socket.roomId)
-        socket.emit("receiveMessage", data);  // 상대방한테
+        socket.emit("receiveMessage", data); // 상대방한테
         socket.to(socket.roomId).emit("receiveMessage", data); // 나한테
       });
       socket.on("disconnect", function () {
