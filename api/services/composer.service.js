c7be25d89183b0deac3eea5cb735f025cd56dbdd
DONGYUN SHIN <windsdy1@gmail.com>
2023-03-24 11:55:45 +0900

    fix: 작곡가 tag 수정, 생성하기

--- api/services/composer.service.js
+++ api/services/composer.service.js
@@ -5,7 +5,7 @@ class ComposerService {
   constructor() {
     this.composerRepository = new ComposerRepository();
   }
-  //코멘트 조회하기
+  //작곡가 조회하기
   getComposer = async ({ composer }) => {
     const result = await this.composerRepository.getComposer({
       composer,
@@ -18,6 +18,14 @@ class ComposerService {
     }
     return result;
   };
-}
 
+  //작곡가 수정하기
+  updateTagComposer = async ({ composer, tag }) => {
+    await this.composerRepository.updateTagComposer({
+      composer,
+      tag,
+    });
+    return { message: "태그가 수정되었습니다." };
+  };
+}
 module.exports = ComposerService;
