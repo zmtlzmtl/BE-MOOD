365ff308c151eea0a08103a8e806a147cca2a509
MOZZ\admin <yeong0319@naver.com>
2023-03-27 20:36:49 +0900

    refactor: controller 부분에 try catch문 빠진것들 다 추가

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -66,70 +66,100 @@ class UserController {
   };
 
   userInfo = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const userInfo = await this.userService.userInfo(userId);
-    res
-      .status(200)
-      .json({ message: "사용자 정보 조회를 성공했습니다.", userInfo });
+    try {
+      const { userId } = res.locals.user;
+      const userInfo = await this.userService.userInfo(userId);
+      res
+        .status(200)
+        .json({ message: "사용자 정보 조회를 성공했습니다.", userInfo });
+    } catch (error) {
+      next(error);
+    }
   };
 
   likeList = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const likeList = await this.userService.likeList(userId);
-    console.log(likeList);
-    res.status(200).json({
-      message: "사용자가 좋아요한 음악조회를 성공했습니다.",
-      likeList,
-    });
+    try {
+      const { userId } = res.locals.user;
+      const likeList = await this.userService.likeList(userId);
+      console.log(likeList);
+      res.status(200).json({
+        message: "사용자가 좋아요한 음악조회를 성공했습니다.",
+        likeList,
+      });
+    } catch (error) {
+      next(error);
+    }
   };
 
   scrapList = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const scrapList = await this.userService.scrapList(userId);
-    console.log(scrapList);
-    res.status(200).json({
-      message: "사용자가 스크랩한 음악조회를 성공했습니다.",
-      scrapList,
-    });
+    try {
+      const { userId } = res.locals.user;
+      const scrapList = await this.userService.scrapList(userId);
+      console.log(scrapList);
+      res.status(200).json({
+        message: "사용자가 스크랩한 음악조회를 성공했습니다.",
+        scrapList,
+      });
+    } catch (error) {
+      next(error);
+    }
   };
 
   uploadProfile = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const file = req.files[0];
+    try {
+      const { userId } = res.locals.user;
+      const file = req.files[0];
 
-    if (!file || req.files.length !== 1) {
-      res
-        .status(400)
-        .json({ message: "프로필사진은 1장만 업로드 가능합니다." });
-    }
-    if (file.mimetype.includes("image") === false) {
-      res
-        .status(400)
-        .json({ massage: "이미지 파일 형식만 업로드 가능합니다." });
-    }
-    const data = await this.userService.uploadImage(file);
+      if (!file || req.files.length !== 1) {
+        res
+          .status(400)
+          .json({ message: "프로필사진은 1장만 업로드 가능합니다." });
+      }
+      if (file.mimetype.includes("image") === false) {
+        res
+          .status(400)
+          .json({ massage: "이미지 파일 형식만 업로드 가능합니다." });
+      }
+      const data = await this.userService.uploadImage(file);
 
-    const fileName = data.Key;
-    await this.userService.uploadProfile(userId, fileName);
-    res.status(200).json({ message: "업로드성공" });
+      const fileName = data.Key;
+      await this.userService.uploadProfile(userId, fileName);
+      res.status(200).json({ message: "업로드성공" });
+    } catch (error) {
+      next(error);
+    }
   };
 
   reviewList = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const data = await this.userService.reviewList(userId);
-    res.status(200).json({ message: "리뷰 조회 성공", reviesList: data });
+    try {
+      const { userId } = res.locals.user;
+      const data = await this.userService.reviewList(userId);
+      res.status(200).json({ message: "리뷰 조회 성공", reviesList: data });
+    } catch (error) {
+      next(error);
+    }
   };
 
   recommentList = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const data = await this.userService.recommentList(userId);
-    res.status(200).json({ message: "대댓글 조회 성공", recommentList: data });
+    try {
+      const { userId } = res.locals.user;
+      const data = await this.userService.recommentList(userId);
+      res
+        .status(200)
+        .json({ message: "대댓글 조회 성공", recommentList: data });
+    } catch (error) {
+      next(error);
+    }
   };
 
   deleteUser = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    await this.userService.deleteUser(userId);
-    res.status(200).json({ message: "회원탈퇴에 성공하였습니다" });
+    try {
+      const { userId } = res.locals.user;
+      await this.userService.deleteUser(userId);
+      res.status(200).json({ message: "회원탈퇴에 성공하였습니다" });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
