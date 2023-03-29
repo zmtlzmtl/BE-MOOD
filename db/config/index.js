2be91067d6e9d77f75cb7c186154df7bbdbe401f
kwakseungmin23 <lookingforfandl@gmail.com>
2023-03-29 18:07:54 +0900

    none

--- db/config/index.js
+++ db/config/index.js
@@ -6,18 +6,18 @@ const development = {
   host: process.env.DEV_DB_HOST,
   dialect: process.env.DEV_DB_DIALECT,
 };
-const test = {
-  username: process.env.TEST_DB_USERNAME,
-  password: process.env.TEST_DB_PASSWORD,
-  database: process.env.TEST_DB_DATABASE,
-  host: process.env.TEST_DB_HOST,
-  dialect: process.env.TEST_DB_DIALECT,
-};
-const production = {
-  username: process.env.PROD_DB_USERNAME,
-  password: process.env.PROD_DB_PASSWORD,
-  database: process.env.PROD_DB_DATABASE,
-  host: process.env.PROD_DB_HOST,
-  dialect: process.env.PROD_DB_DIALECT,
-};
+// const test = {
+//   username: process.env.TEST_DB_USERNAME,
+//   password: process.env.TEST_DB_PASSWORD,
+//   database: process.env.TEST_DB_DATABASE,
+//   host: process.env.TEST_DB_HOST,
+//   dialect: process.env.TEST_DB_DIALECT,
+// };
+// const production = {
+//   username: process.env.PROD_DB_USERNAME,
+//   password: process.env.PROD_DB_PASSWORD,
+//   database: process.env.PROD_DB_DATABASE,
+//   host: process.env.PROD_DB_HOST,
+//   dialect: process.env.PROD_DB_DIALECT,
+// };
 module.exports = { development, test, production };
