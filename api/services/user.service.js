34863ee5df27091a1594d7a3781b3a0dba8ebca2
MOZZ\admin <yeong0319@naver.com>
2023-04-10 08:52:05 +0900

    hotfix: 리프래시토큰 api middleware처럼 받는걸로 변경, middleware에 불필요한 콘솔로그 수정

--- api/services/user.service.js
+++ api/services/user.service.js
@@ -73,9 +73,7 @@ class UserService {
     const accessToken = jwt.sign(
       { userId: login.userId },
       process.env.ACCESS_SECRET_KEY,
-      {
-        expiresIn: "1h",
-      }
+      { expiresIn: "1h" }
     );
     const refreshToken = jwt.sign(
       { userId: login.userId },
@@ -296,8 +294,8 @@ class UserService {
     return;
   };
 
-  findUserIdAndCheckUser = async (refreshToken) => {
-    const User = jwt.verify(refreshToken, process.env.REFRESH_SECRET_KEY);
+  findUserIdAndCheckUser = async (token) => {
+    const User = jwt.verify(token, process.env.REFRESH_SECRET_KEY);
     const finsUser = await this.userRepository.findUser(User.userId);
     if (!finsUser) {
       throw new makeError({
