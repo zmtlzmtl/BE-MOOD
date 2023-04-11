0a72b09fc377875cb9b3464bad926cc87ceecdd3
MOZZ\admin <yeong0319@naver.com>
2023-04-11 14:12:34 +0900

    fix: 닉네임변경시 chat에 있는 닉네임 변경, 회원탈퇴 로직수정

--- db/migrations/20230323053416-create-streamings.js
+++ db/migrations/20230323053416-create-streamings.js
@@ -16,7 +16,7 @@ module.exports = {
           model: "Users",
           key: "userId",
         },
-        onDelete: "NO ACTION",
+        onDelete: "CASCADE",
       },
       musicId: {
         allowNull: false,
